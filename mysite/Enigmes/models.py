45e7e3607ef9889575b2b9e67da299b68f614f6b
Kaelios <rrooxxaass@hotmail.fr>
2017-03-07 10:13:40 +0100

    début de la fusion de nos travaux

---
+++ mysite/Enigmes/models.py
@@ -0,0 +1,37 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+# Create your models here.
+
+class Enigmes(models.Model):
+
+    question = models.CharField(max_length=1000)
+
+    reponse = models.CharField(max_length=50)
+
+    numero = models.IntegerField()
+
+    auteur = models.CharField(max_length=20)
+    
+
+    def __str__(self):
+
+        return self.question
+
+class Joueur(models.Model):
+
+    Pseudo = models.CharField(max_length=20)
+
+    mdp = models.CharField(max_length=50)
+
+    niveau = models.IntegerField()
+
+    adr = models.EmailField(max_length=40)
+
+    master = models.BooleanField()
+
+    def __str__(self):
+
+        return self.adr
+
