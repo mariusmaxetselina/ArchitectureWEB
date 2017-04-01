737e58e27f9b0d109a935c48701361ab11f2c6e4
PNalex <lelexpn@gmail.com>
2017-04-01 18:45:29 +0200

    modifs list_enigmes

--- mysite/Enigmes/views.py
+++ mysite/Enigmes/views.py
@@ -23,8 +23,8 @@ def list_enigmes(request, titreQ):
         rock = Enigmes.objects.get(numero=titreQ)
     except Enigmes.DoesNotExist:
         raise Http404("Globiboulga")
-    return render(request, 'Enigmes/maenigme.html', {'question': rock})
-
+    return render(request, 'Enigmes/enigmes.html', {'question': rock}) #'Enigmes/maenigme.html pour tester sans etre connecte 
+    #Il faut se connecte sur http://127.0.0.1:8000/admin/ 
 
 def connexion(request):
     if request.method == 'POST':
